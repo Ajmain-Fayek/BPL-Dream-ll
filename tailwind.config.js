3ee4fbae02c3b1075c6cf6d1d8983244c2bdfaaf
Ajmain Fayek <fayekajmain0@gmail.com>
2024-10-25 14:56:22 +0600

    colors are configured'

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
             fontFamily: {
                 sora: "'Sora', sans-serif",
             },
+            colors: {
+                "btn-primary": "#E7FE29",
+                "text-primary-black": "#131313",
+                "text-secondary-black": "rgba(19, 19, 19, 0.7) ",
+                "text-primary-white": "rgb(255, 255, 255)",
+                "text-secondary-white": "rgba(255, 255, 255, 0.7)",
+                "title-bg": "rgba(19, 19, 19, 0.05)",
+                "footer-bg": "rgb(6, 9, 26)",
+            },
         },
     },
     plugins: [require("daisyui")],
